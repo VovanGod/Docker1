5e8624e3001d6a021d83c47eaa2ab7f9c01aae34
Vladimir <snegovikkvolodya@yandex.ru>
2024-12-24 18:30:05 +0300

    push all files

---
+++ init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE boxers(
+    rating int,
+    first_name varchar(20),
+    last_name varchar(30),
+    nickName varchar(20)
+);
+
+INSERT INTO boxers (rating, first_name, last_name, nickName) 
+VALUES 
+(1, 'Genadiy', 'Golovkin', 'GGG'),
+(2, 'Денис', 'Лебедев', 'The Hunter'),
+(3, 'Александр', 'Поветкин', 'Russian Vityaz'),
+(4, 'Сергей','Ковалев', 'Krusher'),
+(5, 'Василий', 'Ломаченко', 'The Matrix');
